0c37e583f9e1b9c205f31cf2113334e8a419b00f
ShackMan2000 <julianpieper@gmx.de>
2019-09-14 14:39:08 +0200

    MenuScene, GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+
+
+
+    public int maxLifes, currentLifes;
+
+    public event Action GameOverEvent = delegate { };
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void NewGame()
+    {
+        currentLifes = maxLifes;
+    }
+
+
+    [ContextMenu("HitPig")]
+    public void OnHitPig()
+    {
+        currentLifes--;
+        if (currentLifes == 0)
+            GameOverEvent();
+    }
+
+
+
+}
---
+++ Assets/Scripts/TESTEventListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTEventListener : MonoBehaviour
+{
+
+
+    private void OnEnable()
+    {
+        GameManager.Instance.GameOverEvent += ImDead;
+    }
+    private void OnDisable()
+    {
+        GameManager.Instance.GameOverEvent -= ImDead;
+    }
+
+
+    private void ImDead()
+    {
+
+    }
+
+}
