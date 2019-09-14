2aff37303c9f94b521368437990927e55102a5bc
ShackMan2000 <julianpieper@gmx.de>
2019-09-14 15:38:28 +0200

    PigSpawner

---
+++ Assets/Scripts/MockPig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockPig : MonoBehaviour
+{
+   
+    [SerializeField]
+    private List<Vector3SO> possibleTargets;
+
+    
+
+
+    private void Awake()
+    {
+       
+    }
+
+
+
+
+    public void Activate()
+    {
+
+    }
+}
--- Assets/Scripts/PigSpawner.cs
+++ Assets/Scripts/PigSpawner.cs
@@ -5,31 +5,74 @@ using UnityEngine;
 public class PigSpawner : MonoBehaviour
 {
     [SerializeField]
-    private float SpawnRange, spawnPerSecond;
+    private float spawnRange, spawnPerSecond;
+
+    [SerializeField]
+    private MockPig pigPrefab;
+
+    [SerializeField]
+    private List<Vector3SO> playerPositions;
+
+    [SerializeField]
+    private float spawnHeight;
 
 
-    public List<GameObject>
-
-
-   // private float spawnCounter;
+    public List<MockPig> activePigs, inactivePigs;
 
 
 
-    //private void Update()
-    //{
-    //    spawnCounter += Time.deltaTime;
-    //    if(spawnCounter > 1.0f / spawnPerSecond)
-    //    {
 
-    //    }
-    //}
-
-    //private void SpawnPig()
-    //{
-
-    //}
+    private float spawnCounter;
 
 
+
+    private void Update()
+    {
+        spawnCounter += Time.deltaTime;
+        if (spawnCounter > 1.0f / spawnPerSecond)
+        {
+            SpawnPig();
+        }
+    }
+
+
+
+
+    private void SpawnPig()
+    {
+        MockPig newPig;
+
+        if (inactivePigs.Count > 0)
+            newPig = inactivePigs[0];
+
+        else
+            newPig = Instantiate(pigPrefab);
+
+        newPig.transform.position = ChooseSpawnPosition();
+    }
+
+
+
+    private Vector3 ChooseSpawnPosition()
+    {
+        Vector3 newSpawnPosition;
+        Vector2 pointInCircle = Vector2.zero;
+
+        if (playerPositions.Count > 1)
+        {
+            //flip a coin
+        }
+        else
+        {
+             pointInCircle = new Vector2(playerPositions[0].playerPosition.x, playerPositions[0].playerPosition.z) + Random.insideUnitCircle * spawnRange;
+        }
+
+
+        return new Vector3(pointInCircle.x, spawnHeight, pointInCircle.y);
+
+
+
+    }
 
 
 
